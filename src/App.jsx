5794e92c138b26d5b290efa4fc281b28d33bca94
Your Name <you@example.com>
2025-10-27 11:33:29 +0100

    complete milestone 1 that show product.name and product.price

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,22 @@ import './App.css'
 
 function App() {
 
-
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
   return (
     <>
-
+      <ul className="product-list">
+        {products.map((product, index) => (
+          <li key={index}>
+            <span>{product.name}</span>
+            <span>€{product.price}</span>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
