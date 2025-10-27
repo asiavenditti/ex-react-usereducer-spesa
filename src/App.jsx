7a9d485b4891bf57f2ef0b6392e54d05e63f489c
Your Name <you@example.com>
2025-10-27 12:38:12 +0100

    complete milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-
+import { useState } from 'react';
 function App() {
 
   const products = [
@@ -9,13 +9,24 @@ function App() {
     { name: 'Latte', price: 1.0 },
     { name: 'Pasta', price: 0.7 },
   ];
+
+  const [addedProduct, setAddedProduct] = useState([])
+
+
+  function addToCart(product) {
+    setAddedProduct([...addedProduct, { ...product, quantity: 1 }])
+    console.log(addedProduct)
+  }
+
+
   return (
     <>
       <ul className="product-list">
         {products.map((product, index) => (
           <li key={index}>
-            <span>{product.name}</span>
+            <span>{product.name}</span> -
             <span>€{product.price}</span>
+            <button onClick={() => addToCart(product)}>Aggiungi al carrello</button>
           </li>
         ))}
       </ul>
